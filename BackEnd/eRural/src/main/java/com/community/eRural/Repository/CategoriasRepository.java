739bd3fd93b8d5b60fea818f7b4dd577c1a91b06
Wellington <wellingtonsouza2504@gmail.com>
2021-03-22 15:06:28 -0300

    Model Categoria e Produtos

---
+++ BackEnd/eRural/src/main/java/com/community/eRural/Repository/CategoriasRepository.java
@@ -0,0 +1,13 @@
+package com.community.eRural.Repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.community.eRural.model.Categorias;
+
+@Repository
+public interface CategoriasRepository extends JpaRepository<Categorias,Long>
+	{
+	public List<Categorias>findAllByNomeContainingIgnoreCase(String nome);
+	}
